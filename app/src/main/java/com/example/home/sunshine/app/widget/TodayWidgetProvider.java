abce23cfafb486954600775fe90876c37945dc21
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-05-12 16:17:58 -0700

    Start building widget

---
+++ app/src/main/java/com/example/home/sunshine/app/widget/TodayWidgetProvider.java
@@ -0,0 +1,17 @@
+package com.example.home.sunshine.app.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * @author Prateesh Goswami
+ * @version 1.0
+ * @date 4/29/2016
+ */
+public class TodayWidgetProvider extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
